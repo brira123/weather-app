c68fc3b80f3672d63721cdc2532151e78e3e5717
brira123 <39010769+brira123@users.noreply.github.com>
2018-08-29 07:50:33 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
 function getWeather() {
     document.querySelector(".weather-info").style.display = "block";
     const cityName = document.querySelector("input").value;
@@ -54,7 +57,36 @@ function getWeather() {
         }
     });
     $.ajax({
-         url:`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&mode=xml&appid=b4397e351670510cacd5a62924137601`,
+         url:`https://api.openweathermap.org/data/2.5/forecast?q=karachi,pk&appid=b4397e351670510cacd5a62924137601&units=metric`,
+         success: function (data) {
+            console.log(data);
          
+            var d1 = new Date(data.list[0].dt*1000); 
+            var d2 = new Date(data.list[8].dt*1000); 
+            var d3 = new Date(data.list[16].dt*1000); 
+            var d4 = new Date(data.list[24].dt*1000); 
+            var d5 = new Date(data.list[32].dt*1000); 
+     
+            document.querySelector(".day1").innerHTML = days[d1.getDay()]; 
+            document.querySelector(".day2").innerHTML = days[d2.getDay()];        
+            document.querySelector(".day3").innerHTML = days[d3.getDay()];        
+            document.querySelector(".day4").innerHTML = days[d4.getDay()];  
+            document.querySelector(".day5").innerHTML = days[d5.getDay()]; 
+    
+            document.querySelector(".date-one").innerHTML = `${months[d1.getMonth()]} ${d1.getDate()}`;        
+            document.querySelector(".date-two").innerHTML = `${months[d2.getMonth()]} ${d2.getDate()}` ;        
+            document.querySelector(".date-three").innerHTML = `${months[d3.getMonth()]} ${d3.getDate()}`;        
+            document.querySelector(".date-four").innerHTML = `${months[d4.getMonth()]} ${d4.getDate()}`;        
+            document.querySelector(".date-five").innerHTML = `${months[d5.getMonth()]} ${d5.getDate()}`;        
+    
+            document.querySelector(".temperature-one").innerHTML = Math.round(data.list[0].main.temp) + " C°";        
+            document.querySelector(".temperature-two").innerHTML = Math.round(data.list[5].main.temp) + " C°";          
+            document.querySelector(".temperature-three").innerHTML = Math.round(data.list[13].main.temp) + " C°";         
+            document.querySelector(".temperature-four").innerHTML = Math.round(data.list[21].main.temp) + " C°";         
+            document.querySelector(".temperature-five").innerHTML = Math.round(data.list[29].main.temp) + " C°";      
+        },
+        error: function(error){
+            console.log(error)
+        }
     })
 }
